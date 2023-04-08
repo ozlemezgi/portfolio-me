5c4d8be7f8c13a5da2ac76e00f4401f921fb461a
ozlemezgi <ozlemezgisari@icloud.com>
2023-04-08 23:16:14 +0300

    deneme

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,24 +30,35 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <Layout title={"Özlem Ezgi Sarı"} description={"My Portfolio"}>
-      <motion.img src="/images/ball-1.svg" alt="Ball-1" className="hidden md:block h-auto absolute bottom-10 left-10 z-[1]"
+      <motion.img
+        src="/images/ball-1.svg"
+        alt="Ball-1"
+        className="hidden md:block h-auto absolute bottom-10 left-10 z-[1]"
         whileHover={{ y: -10, rotate: 90, transition: { duration: 0.4 } }}
         drag
         dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
       />
-      <motion.img src="/images/ball-2.svg" alt="Ball-2" className="hidden md:block h-auto absolute top-48 right-10 z-[1]"
+      <motion.img
+        src="/images/ball-2.svg"
+        alt="Ball-2"
+        className="hidden md:block h-auto absolute top-48 right-10 z-[1]"
         whileHover={{ y: -10, rotate: 90, transition: { duration: 0.4 } }}
         drag
         dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
       />
       <main className="w-full h-full">
         <section className="h-full max-w-5xl mx-auto pt-16 md:pt-8">
-          <div className="flex flex-col items-center justify-center md:items-start"><h4 className="text-lg text-gray-800 dark:text-white"><span className="text-xl font-normal">Hello,</span>I'am</h4><h1 className="mt-5 text-xl font-extrabold tracking-normal text-gray-800 uppercase md:text-4xl md:mt-3 dark:text-white">Özlem Ezgİ SARI</h1>
+          <div className="flex flex-col items-center justify-center md:items-start">
+            <h4 className="text-lg text-gray-800 dark:text-white">
+              <span className="text-xl font-normal">Hello,</span>I'am
+            </h4>
+            <h1 className="mt-5 text-xl font-extrabold tracking-normal text-gray-800 uppercase md:text-4xl md:mt-3 dark:text-white">
+              Özlem Ezgİ SARI
+            </h1>
 
             {/* Animation Start */}
             <div className="mt-5 text-2xl font-normal text-transparent bg-gradient-to-r from-pink-500 via-indigo-400 to-purple-700 bg-clip-text md:mt-3">
               {/* <TypingAnimation></TypingAnimation> */}
-
 
               {/* <Typewriter
        onInit={(typewriter)=>{typewriter
@@ -61,7 +72,11 @@ export default function Home() {
 
               <Typewriter
                 options={{
-                  strings: ['Computer Engineer', 'Software Developer', "Cat Mom 🐈"],
+                  strings: [
+                    "Computer Engineer",
+                    "Software Developer",
+                    "Cat Mom 🐈",
+                  ],
                   autoStart: true,
                   loop: true,
                 }}
@@ -72,17 +87,26 @@ export default function Home() {
 
             {/* github */}
             <div className="flex items-center mt-8 space-x-6 md:mt-4">
-              <motion.a href="https://github.com/ozlemezgi" target={"_blank"} rel="noopener noreferrer" className="flex items-center justify-center text-gray-800 transition-colors duration-300 dark:text-gray-100 dark:hover:text-pinkish-200 hover:text-pinkish-200"
-              whileHover={{scale:1.1}}>
-                <GitHubIcon className={"w-8 h-8 fill-current"}/>
+              <motion.a
+                href="https://github.com/ozlemezgi"
+                target={"_blank"}
+                rel="noopener noreferrer"
+                className="flex items-center justify-center text-gray-800 transition-colors duration-300 dark:text-gray-100 dark:hover:text-pinkish-200 hover:text-pinkish-200"
+                whileHover={{ scale: 1.1 }}
+              >
+                <GitHubIcon className={"w-8 h-8 fill-current"} />
               </motion.a>
 
               {/* linkedin */}
-              <motion.a href="https://www.linkedin.com/in/ozlemezgisari/" target={"_blank"} rel="noopener noreferrer" className="flex items-center justify-center text-gray-800 transition-colors duration-300 dark:text-gray-100 dark:hover:text-pinkish-200 hover:text-pinkish-200"
-              whileHover={{scale:1.1}}>
-                <LinkedinIcon className={"w-8 h-8 fill-current"}/>
+              <motion.a
+                href="https://www.linkedin.com/in/ozlemezgisari/"
+                target={"_blank"}
+                rel="noopener noreferrer"
+                className="flex items-center justify-center text-gray-800 transition-colors duration-300 dark:text-gray-100 dark:hover:text-pinkish-200 hover:text-pinkish-200"
+                whileHover={{ scale: 1.1 }}
+              >
+                <LinkedinIcon className={"w-8 h-8 fill-current"} />
               </motion.a>
-
             </div>
             {/* Icons End */}
           </div>
@@ -109,10 +133,13 @@ export default function Home() {
 
                     <div className="">
                       <p className="text-sm">
-                        <span className="text-pink-300"> {">"} </span> const 
+                        <span className="text-pink-300"> {">"} </span> const
                         {/* frontend */}
-                        <span className="font-bold text-pink-300"> frontendSkills</span> =
-                        ["Reactjs", "Nextjs", "Graphql", "Nodejs"];{" "}
+                        <span className="font-bold text-pink-300">
+                          {" "}
+                          frontendSkills
+                        </span>{" "}
+                        = ["Reactjs", "Nextjs", "Graphql", "Nodejs"];{" "}
                       </p>
                       <p className="text-sm">
                         <span className="text-pink-300"> {">"} </span> const
@@ -124,12 +151,12 @@ export default function Home() {
                   </div>
 
                   <div className="flex items-center space-x-8">
-                    <Link href="/projects"
-                       className="transition duration-300">Projects
+                    <Link href="/projects" className="transition duration-300">
+                      Projects
                     </Link>
 
                     <a
-                      href="https://www.youtube.com/channel/UCfGXEo2uCkVqcIqkIaFNGcA"
+                      href="https://github.com/ozlemezgi"
                       target={"_blank"}
                       rel="noopener noreferrer"
                       className="transition duration-300"
@@ -145,8 +172,7 @@ export default function Home() {
         </section>
       </main>
     </Layout>
-
-  )
+  );
 }
 
 
